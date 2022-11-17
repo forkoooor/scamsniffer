544bf2679d6eff58f7000f1f8e074975226f2a47
fun <lljxx1@gmail.com>
2022-11-17 23:17:32 +0800

    web-extension: normalize

--- web-extension/source/core/features/phasing-warning/index.ts
+++ web-extension/source/core/features/phasing-warning/index.ts
@@ -4,12 +4,13 @@ import byPassedOrigin from './byPassedOriginManager';
 
 tabInfoManager.addTabUpdateListener(async ({id, url}) => {
   if (url) {
-    const {hostname, href} = new URL(url);
+    const {hostname, href, protocol} = new URL(url);
     if (byPassedOrigin.has(hostname)) {
       return;
     }
+    const normalizedURL = `${protocol}//${hostname}`;
     const res = await fetch(
-      `https://domain-api.scamsniffer.io/check?url=${url}&api_key=0c1276ff719aece`
+      `https://domain-api.scamsniffer.io/check?url=${encodeURIComponent(normalizedURL)}&api_key=0c1276ff719aece`
     ).then((r) => r.json());
 
     if (res?.status === 'BLOCKED') {
